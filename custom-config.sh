bf43f4b67d6a0fa0f77b8c4da75e70b3b75399da
Claudio Cesar Sanchez Tejeda <demonccc.y@gmail.com>
2025-03-12 22:48:22 -0300

    Update custom-config.sh

--- custom-config.sh
+++ custom-config.sh
@@ -1,34 +1,6 @@
 # Use "make defconfig" to generate a complete .config file
 
-# Custom firmware base
-# echo 'CONFIG_TARGET_x86=y' >> $BUILDROOT/.config
-# echo 'CONFIG_TARGET_x86_64=y' >> $BUILDROOT/.config
-# echo 'CONFIG_TARGET_x86_64_DEVICE_generic=y' >> $BUILDROOT/.config
-# echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=3072' >> $BUILDROOT/.config
-
-#echo 'CONFIG_TARGET_mediatek=y' >> $BUILDROOT/.config
-#echo 'CONFIG_TARGET_mediatek_filogic=y' >> $BUILDROOT/.config
-#echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_bananapi_bpi-r3-mini=y' >> $BUILDROOT/.config
-
-# Add OpenClash
-#echo '# CONFIG_PACKAGE_dnsmasq is not set' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILDROOT/.config
-
-# Add Docker
-#echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILDROOT/.config
-
-# Add other apps
-echo 'CONFIG_PACKAGE_luci=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_luci-ssl=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_luci-app-wol=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
-#echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
-
-# QCN550x Archer A9 v6
+# Custom firmware for QCN550x Archer A9 v6
 
 echo 'CONFIG_TARGET_ath79=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_ath79_generic=y' >> $BUILDROOT/.config
@@ -40,6 +12,7 @@ echo 'CONFIG_PACKAGE_e2fsprogs=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_gawk=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_getdns=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_htop=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_ip-full=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_iptables-mod-ipopt=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_iptables-nft=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_irqbalance=y' >> $BUILDROOT/.config
@@ -53,6 +26,7 @@ echo 'CONFIG_PACKAGE_kmod-ipt-core=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_kmod-ipt-ipopt=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_kmod-lib-crc16=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_kmod-nf-ipt=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_kmod-nft-bridge=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_kmod-nft-compat=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_kmod-sched-cake=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_kmod-sched-core=y' >> $BUILDROOT/.config
@@ -89,10 +63,12 @@ echo 'CONFIG_PACKAGE_libxtables=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_libyaml=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-app-firewall=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_luci-app-mosquito=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-app-opkg=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-app-sqm=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-app-ttyd=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-base=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-light=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-mod-admin-full=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-mod-network=y' >> $BUILDROOT/.config
@@ -100,9 +76,16 @@ echo 'CONFIG_PACKAGE_luci-mod-status=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-mod-system=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-proto-ipv6=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-proto-ppp=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_luci-proto-relay=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-proto-wireguard=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-theme-bootstrap=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_mesh11sd=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_mosquitto-ssl=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_mosquitto-client-ssl=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_libmosquitto-ssl=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_parted=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_px5g-mbedtls=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_relayd=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_rpcd=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_rpcd-mod-file=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_rpcd-mod-iwinfo=y' >> $BUILDROOT/.config
@@ -119,8 +102,10 @@ echo 'CONFIG_PACKAGE_ucode-mod-math=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_uhttpd=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_uhttpd-mod-ubus=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_usbutils=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_vxlan=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_wireguard-tools=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_wpad-mbedtls=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_xtables-nft=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_zlib=y' >> $BUILDROOT/.config
 echo 'CONFIG_PARTED_READLINE=y' >> $BUILDROOT/.config
